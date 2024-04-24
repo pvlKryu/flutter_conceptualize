0187f3c2134a04b43bcd99004d61ad145eafe6ed
@kryuchkov_ps <kryuchkov_ps@msk.atb.su>
2024-04-24 18:13:24 +1000

    feature - UI main screen

--- lib/presentation/home_screen/main_screen.dart
+++ lib/presentation/home_screen/main_screen.dart
@@ -12,7 +12,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final TextEditingController _textEditingController = TextEditingController();
+  final _textEditingController = TextEditingController();
 
   @override
   void dispose() {
@@ -30,84 +30,75 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Discover Concepts'),
+        leading: const Icon(Icons.history),
         backgroundColor: Colors.blue,
       ),
-      body: SafeArea(
-        child: BlocBuilder<MainScreenBloc, MainScreenState>(builder: (context, state) {
-          return SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextField(
-                    controller: _textEditingController,
-                    decoration: const InputDecoration(
-                      labelText: 'Search',
-                      hintText: 'Enter your topic of interest',
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                      ),
-                    ),
-                    onSubmitted: (_) => _onSearch(),
-                  ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _textEditingController,
+              decoration: const InputDecoration(
+                hintText: 'Enter your topic of interest',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: ElevatedButton(
-                    onPressed: _onSearch,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      textStyle: const TextStyle(fontSize: 20),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                    ),
-                    child: const Text('Search', style: TextStyle(color: Colors.black),),
-                  ),
-                ),
-                if (state.isLoading) const CircularProgressIndicator(),
-                if (state.conceptEntitiesList != null) ...[
-                  const ListTile(
-                    title: Text('Suggested Terms in Relevant Order'),
-                  ),
-                  SizedBox(
-                    height: 300,
-                    child: ListView.builder(
-                      itemCount: state.conceptEntitiesList?.length ?? 0,
-                      itemBuilder: (context, index) {
-                        final entity = state.conceptEntitiesList?[index];
-                        return ListTile(
-                          leading: const Icon(Icons.lightbulb_outline),
-                          title: Text('${index + 1} ${entity?.word ?? ''} '),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-                if (state.conceptEntitiesList == null)
-                  const ListTile(
-                    title: Text('Make any search for getting suggested terms'),
-                  ),
-                const ListTile(
-                  title: Text('Recent Searches'),
-                ),
-                SizedBox(
-                  height: 100,
-                  child: ListView.builder(
-                    itemCount: 5, // Placeholder count
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: const Icon(Icons.history),
-                        title: Text('Search ${index + 1}'),
-                      );
-                    },
-                  ),
-                ),
-              ],
+              ),
+              onSubmitted: (_) => _onSearch(),
             ),
-          );
-        }),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ElevatedButton(
+              onPressed: _onSearch,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                textStyle: const TextStyle(fontSize: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+              child: const Text(
+                'Search',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: BlocBuilder<MainScreenBloc, MainScreenState>(
+              builder: (context, state) {
+                if (state.isLoading) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (state.conceptEntitiesList == null) {
+                  return const ListTile(
+                    title: Text('Make any search for getting suggested terms'),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: state.conceptEntitiesList?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    final entity = state.conceptEntitiesList?[index];
+                    return ListTile(
+                      onTap: () => print(''),
+                      title: Row(
+                        children: [
+                          Text(
+                            '${index + 1}. ',
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text('${entity?.word ?? ''} '),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
